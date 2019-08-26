fb16a3b6f3597489c295a17cd7d8acdced7ca041
Stevie Slotterback <steve.slotterback@gmail.com>
2019-08-25 23:17:02 -0400

    Major Overhaul: Segment Into Blueprints and Implement User Logins

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,42 @@
-from flask import Flask
+import os
+from flask import Flask, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_moment import Moment
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-from app import routes, models
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = 'Please log in to access this page.'
+mail = Mail()
+moment = Moment()
+
+
+#Application Factory Function
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app, db)
+    moment.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
+
+
+from app import models
